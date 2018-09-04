be8ecec595727e2317e3a7214d74f497aeb2c1c4
Kimberly Keller <kkeller@xpansiv.com>
2018-09-03 18:22:34 -0600

    wrapped up game condition

--- hangman/hangman_hs.hs
+++ hangman/hangman_hs.hs
@@ -151,17 +151,18 @@ reveal c x
 
 currentWord :: String -> String -> String
 currentWord [] word = map (\x -> '.') word
-currentWord guess word = concat [[reveal c x | x <- word] | c <- guess]
+currentWord guess word = foldr mergeString [] list where list = [[reveal c x | x <- word] | c <- guess]
 
-currentWord' :: String -> String -> [String]
-currentWord' [] word = [map (\x -> '.') word]
-currentWord' guess word = foldr (:) [] list where list = [[reveal c x | x <- word] | c <- guess]
+mergeString :: String -> String -> String
+mergeString [] [] = []
+mergeString x [] = x
+mergeString [] x = x
+mergeString (x:xs) (y:ys) = (compareChar x y) : (mergeString xs ys)
 
-compare' :: Char -> Char -> Char
-compare' = undefined 
--- compare' ('.') x = x
--- compare' x ('.') = x
--- compare' _ _ = '.'
+compareChar :: Char -> Char -> Char
+compareChar ('.') x = x 
+compareChar x ('.') = x
+compareChar _ _ = '.'
 
 getListLength :: String -> Int
 getListLength s = length $ splitOn "|" s
@@ -179,7 +180,9 @@ checkAlpha s = all (==True) (map isLetter s)
 checkInput :: IO String
 checkInput = do
                 line <- getLine
-                if checkAlpha line then return line else checkInput
+                if checkAlpha line then return line else do 
+                                                            putStrLn ("Only alphanumeric symbols are allowed (a-z, A-Z), try again:\n")
+                                                            checkInput 
 
 getHangman :: Int -> String
 getHangman n 
@@ -195,59 +198,70 @@ getHangman n
             | n == 9 = case9
             | n == 10 = case10
 
-{- 
- State of the world:
-    Current word
-    Guess word
-    Number of incorrect guess
-    Incorrectly guessed letters
-    If the game is won or not
-    Partially revealed word 
--}
+getFst :: (a, b, c, d) -> a
+getFst (a, _, _, _) = a
 
-getFst :: (a, b, c) -> a
-getFst (a, _, _) = a
+getSnd :: (a, b, c, d) -> b
+getSnd (_, b, _, _) = b
 
-getSnd :: (a, b, c) -> b
-getSnd (_, b, _) = b
+getThrd ::(a, b, c, d) -> c
+getThrd (_, _, c, _) = c
 
-getThrd :: (a, b, c) -> c
-getThrd (_, _, c) = c
+getFrth :: ( a, b, c, d) -> d
+getFrth (_, _, _, d) = d
 
+resultsTitle:: String
+resultsTitle = "---------------\n" ++
+               "--- Results ---\n" ++
+               "---------------\n\n"
 
-playGame' :: Int -> Int -> String -> String -> String -> IO (Int, Int, String)
-playGame' turn n [] word partialWord = return (turn, n, partialWord)
-playGame' turn n (c:cs) word partialWord 
+results :: Bool -> String -> String
+results (True) word = resultsTitle ++ "Congratulations you guessed the right word!\n\n"
+results (False) word = resultsTitle ++ "You guessed the wrong word. The word was "  ++ word ++ " better luck next time!\n\n"
+
+-- First Bool is won the game T / F Second Bool is game over T / F
+checkGame :: Int -> String -> String -> (Bool, Bool)
+checkGame (10) partialWord word = (False, True)
+checkGame wrongGuesses partialWord word 
+                                    | partialWord == word = (True, True)
+                                    | otherwise = (False, False)
+
+playGame' :: Int -> Int -> String -> String -> String -> (Bool, Bool) -> IO (Int, Int, String, (Bool, Bool))
+playGame' turn n [] word partialWord status = return (turn, n, partialWord, status)
+playGame' turn n (c:cs) word partialWord status
                     | checkGuess c word = do
-                                            putStrLn $ show turn 
                                             putStrLn ("\nThat letter was correct.\n")
                                             putStrLn ("The word including the letters you guessed: " ++ (currentWord (c:partialWord) word) ++ "\n")
-                                            putStrLn (c:partialWord)
+                                            let check = checkGame n (currentWord (c:partialWord) word) word
                                             putStrLn $ getHangman n
-                                            playGame' (turn+1) n cs word (c:partialWord)
+                                            if check == (True, True) || check == (False, True) 
+                                                then return (turn, n, (c:partialWord), check)
+                                                else playGame' (turn+1) n cs word (c:partialWord) (False, False)
                     | otherwise = do
-                                     putStrLn $ show turn 
                                      putStrLn ("\nThat letter was incorrect.\n")
                                      putStrLn ("The word including the letters you guessed: " ++ (currentWord partialWord word) ++ "\n")
+                                     let check = checkGame (n+1) (currentWord partialWord word) word
                                      putStrLn $ getHangman $ n+1
-                                     putStrLn (partialWord)
-                                     playGame' (turn+1) (n+1) cs word partialWord
+                                     if check == (True, True) || check == (False, True) 
+                                                then return ((turn+1), (n+1), partialWord, check)
+                                                else playGame' (turn+1) (n+1) cs word partialWord (False, False)
 
  -- play the game for each character in the line
-playGame :: Int -> Int -> String -> String -> IO ()
-playGame turn n partialWord word = do
-                        putStrLn $ show turn ++ ".     Enter the letter(s) you want to guess: "
-                        line  <- checkInput
-                        state <- playGame' turn n line word partialWord
-                        playGame (getFst state) (getSnd state) (getThrd state) word
-
+playGame :: Int -> Int -> String -> (Bool, Bool) -> String -> IO ()
+playGame turn n partialWord status word = do
+                                    putStrLn $ show turn ++ ".     Enter the letter(s) you want to guess: "
+                                    line  <- checkInput
+                                    state <- playGame' turn n line word partialWord status
+                                    let check = getFrth state
+                                    if check == (True, True) || check == (False, True) 
+                                        then putStrLn (results (fst check) word)
+                                        else playGame (getFst state) (getSnd state) (getThrd state) (getFrth state) word
 
 main = do 
         putStrLn logo
         s <- readFile "words.txt"
         num <- randomRIO (0, getListLength s) :: IO Int
         putStrLn $ intro $ currentWord [] $ getWord s num
-
-        playGame 0 0 [] (getWord s num)
+        playGame 0 0 [] (False, False) (getWord s num)
 
 
